58c526f2d60d7e29d2a426d0d37c53e2c21e6e32
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2021-12-20 15:20:07 -0300

    config eslint and npm version

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native';
 const Home = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Home!</Text>
+    {foo && foo.bar}
   </View>
 );
 
