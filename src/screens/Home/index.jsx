e21148a3b99102667a2a2909a4914a2598da2492
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2021-12-21 15:21:23 -0300

    fix absolute import

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -4,7 +4,6 @@ import { View, Text } from 'react-native';
 const Home = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Home!</Text>
-    {foo && foo.bar}
   </View>
 );
 
