1d0ee0f68f1a8a2f8093c02ec084c7d9f3c52f85
Gabriel <gabriel.santiago@tqi.com.br>
2022-03-08 14:43:37 -0300

    improve loader ref

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,27 +1,32 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable react/display-name */
-import React, { useState, useRef, useCallback } from 'react';
+import React, {
+  useState,
+  useRef,
+  useCallback,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
 import { View } from 'react-native';
 import LottieView from 'lottie-react-native';
 import styles from './style';
 
-let refs = [];
+let currentRef;
 
-const addNewRef = newRef =>
-  refs.push({
-    current: newRef,
-  });
-
-const removeOldRef = oldRef => {
-  refs = refs.filter(r => r.current !== oldRef);
+const addNewRef = newRef => {
+  currentRef = newRef;
 };
 
-const LoaderRoot = React.forwardRef((props, ref) => {
+const removeOldRef = () => {
+  currentRef = null;
+};
+
+const LoaderRoot = forwardRef((props, ref) => {
   const [loadersCount, setLoadersCount] = useState(0);
   const [show, setShow] = useState(false);
 
-  React.useImperativeHandle(
+  useImperativeHandle(
     ref,
     useCallback(
       () => ({
@@ -59,35 +64,23 @@ function Loader(props) {
   const loaderRef = useRef(null);
 
   const setRef = useCallback(ref => {
-    // Since we know there's a ref, we'll update `refs` to use it.
     if (ref) {
-      // store the ref in this toast instance to be able to remove it from the array later when the ref becomes null.
       loaderRef.current = ref;
       addNewRef(ref);
     } else {
-      // remove the this toast's ref, wherever it is in the array.
-      removeOldRef(loaderRef.current);
+      removeOldRef();
     }
   }, []);
 
   return <LoaderRoot ref={setRef} {...props} />;
 }
 
-const getRef = () => {
-  const reversePriority = [...refs].reverse();
-  const activeRef = reversePriority.find(ref => ref?.current !== null);
-  if (!activeRef) {
-    return null;
-  }
-  return activeRef.current;
-};
-
 Loader.show = () => {
-  getRef()?.show();
+  currentRef?.show();
 };
 
 Loader.hide = () => {
-  getRef()?.hide();
+  currentRef?.hide();
 };
 
-export { Loader as default };
+export default Loader;
