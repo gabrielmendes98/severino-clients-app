46a631198ac7e6efaa2543c8711cb40291691fdd
Gabriel <gabriel.santiago@tqi.com.br>
2022-03-08 16:40:01 -0300

    improve loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,50 +1,32 @@
 /* eslint-disable react/function-component-definition */
-/* eslint-disable react/no-multi-comp */
-/* eslint-disable react/display-name */
-import React, {
-  useState,
-  useRef,
-  useCallback,
-  useImperativeHandle,
-  forwardRef,
-} from 'react';
+import React, { useState, useImperativeHandle } from 'react';
 import { View } from 'react-native';
 import LottieView from 'lottie-react-native';
 import styles from './style';
 
 let currentRef;
-
-const addNewRef = newRef => {
-  currentRef = newRef;
-};
-
-const removeOldRef = () => {
-  currentRef = null;
-};
-
-const LoaderRoot = forwardRef((props, ref) => {
+function Loader() {
   const [loadersCount, setLoadersCount] = useState(0);
   const [show, setShow] = useState(false);
 
   useImperativeHandle(
-    ref,
-    useCallback(
-      () => ({
-        show: () => {
-          setLoadersCount(count => count + 1);
-          setShow(true);
-        },
-        hide: () => {
-          if (loadersCount === 1) {
-            setShow(false);
-            setLoadersCount(count => count - 1);
-          } else {
-            setLoadersCount(count => count - 1);
-          }
-        },
-      }),
-      [setShow, loadersCount],
-    ),
+    ref => {
+      currentRef = ref;
+    },
+    () => ({
+      show: () => {
+        setLoadersCount(count => count + 1);
+        setShow(true);
+      },
+      hide: () => {
+        if (loadersCount === 1) {
+          setShow(false);
+          setLoadersCount(count => count - 1);
+        } else {
+          setLoadersCount(count => count - 1);
+        }
+      },
+    }),
   );
 
   return (
@@ -58,21 +40,6 @@ const LoaderRoot = forwardRef((props, ref) => {
       </View>
     )
   );
-});
-
-function Loader(props) {
-  const loaderRef = useRef(null);
-
-  const setRef = useCallback(ref => {
-    if (ref) {
-      loaderRef.current = ref;
-      addNewRef(ref);
-    } else {
-      removeOldRef();
-    }
-  }, []);
-
-  return <LoaderRoot ref={setRef} {...props} />;
 }
 
 Loader.show = () => {
