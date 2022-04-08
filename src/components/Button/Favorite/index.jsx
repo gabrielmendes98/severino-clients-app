9437519bebf9577839da446606346616205ef78e
Gabriel <gabriel.santiago@tqi.com.br>
2022-04-08 15:42:57 -0300

    increase whatsapp and favorite buttons touch area

--- src/components/Button/Favorite/index.jsx
+++ src/components/Button/Favorite/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 import theme from 'common/styles/theme';
 import professionalsService from 'api/services/professionals';
@@ -13,22 +14,26 @@ const FavoriteButton = ({ workerId, style }) => {
       .favorite(workerId)
       .then(({ favorited }) => updateFavorite(workerId, favorited));
 
-  return favorites[workerId] ? (
-    <FontAwesomeIcons
-      onPress={favorite}
-      color={theme.colors.red}
-      name="heart"
-      size={20}
-      style={style}
-    />
-  ) : (
-    <FontAwesomeIcons
-      onPress={favorite}
-      color={theme.colors.black}
-      name="heart-o"
-      size={20}
-      style={style}
-    />
+  return (
+    <TouchableWithoutFeedback onPress={favorite}>
+      <View style={styles.container}>
+        {favorites[workerId] ? (
+          <FontAwesomeIcons
+            color={theme.colors.red}
+            name="heart"
+            size={20}
+            style={style}
+          />
+        ) : (
+          <FontAwesomeIcons
+            color={theme.colors.black}
+            name="heart-o"
+            size={20}
+            style={style}
+          />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -36,5 +41,9 @@ FavoriteButton.propTypes = {
   workerId: PropTypes.string.isRequired,
   style: PropTypes.object,
 };
+
+const styles = StyleSheet.create({
+  container: { padding: 20, margin: -20 },
+});
 
 export default FavoriteButton;
--- src/components/Button/Whatsapp/index.jsx
+++ src/components/Button/Whatsapp/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Linking } from 'react-native';
+import {
+  Linking,
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { WHATSAPP_URL } from 'common/constants';
 import { onlyNumbersFormat } from 'common/util/formatters';
@@ -9,11 +14,21 @@ const WhatsappButton = ({ phone }) => {
   const openWhatsapp = () =>
     Linking.openURL(WHATSAPP_URL + onlyNumbersFormat(phone));
 
-  return <WhatsAppIcon size={21} onPress={openWhatsapp} />;
+  return (
+    <TouchableWithoutFeedback onPress={openWhatsapp}>
+      <View style={styles.container}>
+        <WhatsAppIcon size={21} />
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 WhatsappButton.propTypes = {
   phone: PropTypes.string.isRequired,
 };
+
+const styles = StyleSheet.create({
+  container: { padding: 20, margin: -20 },
+});
 
 export default WhatsappButton;
