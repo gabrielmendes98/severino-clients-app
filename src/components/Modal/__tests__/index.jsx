cbe899bbb31667816afb3a66d18925aff9cb9bc5
Gabriel <gabriel.santiago@tqi.com.br>
2022-04-26 16:21:13 -0300

    simplify modal tests

--- src/components/Modal/__tests__/index.jsx
+++ src/components/Modal/__tests__/index.jsx
@@ -5,6 +5,22 @@ import { View, TouchableWithoutFeedback } from 'react-native';
 import { rawRender, fireEvent, waitFor } from 'test-utils';
 import withModal from '../withModal';
 
+const showModalButtonId = 'showModalButtonId';
+const renderModalBase = config => {
+  const Component = ({ showModal }) => (
+    <View>
+      <TouchableWithoutFeedback
+        testID={showModalButtonId}
+        onPress={() => showModal(config)}
+      >
+        <View></View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+  const ComponentWithModal = withModal(Component);
+  return rawRender(<ComponentWithModal />);
+};
+
 it('should wrap component with modal when use withModal HOC', () => {
   const screenTestId = 'screenTestId';
   const Component = () => <View testID={screenTestId}></View>;
@@ -16,77 +32,43 @@ it('should wrap component with modal when use withModal HOC', () => {
 });
 
 it('should receive showModal prop and call it to show modal passing title and message', () => {
-  const touchableId = 'touchableId';
   const config = {
     title: 'Title test',
     message: 'Message test',
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByTestId, getByText } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { getByTestId, getByText } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   expect(getByText(config.title)).toBeTruthy();
   expect(getByText(config.message)).toBeTruthy();
 });
 
 it('should call onClose callback when close modal', () => {
-  const touchableId = 'touchableId';
   const config = {
     onClose: jest.fn(),
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByTestId } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { getByTestId } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   fireEvent(getByTestId('modal-cancel'), 'press');
   expect(config.onClose).toHaveBeenCalled();
 });
 
 it('should be able to change cancel label', () => {
-  const touchableId = 'touchableId';
   const cancelLabel = 'cancel label test';
   const config = {
     cancelLabel,
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByText, getByTestId } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { getByText, getByTestId } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   expect(getByText(cancelLabel)).toBeTruthy();
 });
 
 it('should be able to pass new action buttons', () => {
-  const touchableId = 'touchableId';
   const config = {
     actions: [
       {
@@ -103,20 +85,9 @@ it('should be able to pass new action buttons', () => {
     ],
   };
 
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByText, getByTestId } = rawRender(<ComponentWithModal />);
+  const { getByText, getByTestId } = renderModalBase(config);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  fireEvent(getByTestId(showModalButtonId), 'press');
 
   const [btnOne, btnTwo] = config.actions;
 
@@ -128,47 +99,25 @@ it('should be able to pass new action buttons', () => {
 });
 
 it('should be able to hide cancel button', () => {
-  const touchableId = 'touchableId';
   const config = {
     cancelButton: false,
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { queryByTestId, getByTestId } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { queryByTestId, getByTestId } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   expect(queryByTestId('modal-cancel')).toBeFalsy();
 });
 
 it('should be able to disable fullscreen and set modal height', () => {
-  const touchableId = 'touchableId';
   const config = {
     fullScreen: false,
     height: 70,
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByTestId } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { getByTestId } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   expect(getByTestId('modal-content')).toHaveStyle({
     height: `${config.height}%`,
     flex: 0,
@@ -179,29 +128,17 @@ it('should be able to pass a component as modal body', () => {
   const bodyTestID = 'bodyTestID';
   const Body = () => <View testID={bodyTestID}></View>;
 
-  const touchableId = 'touchableId';
   const config = {
     body: Body,
   };
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByTestId } = rawRender(<ComponentWithModal />);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  const { getByTestId } = renderModalBase(config);
+
+  fireEvent(getByTestId(showModalButtonId), 'press');
   expect(getByTestId(bodyTestID)).toBeTruthy();
 });
 
 it('should keep modal open when pass skip close to action', async () => {
-  const touchableId = 'touchableId';
   const config = {
     actions: [
       {
@@ -214,20 +151,9 @@ it('should keep modal open when pass skip close to action', async () => {
     onClose: jest.fn(),
   };
 
-  const Component = ({ showModal }) => (
-    <View>
-      <TouchableWithoutFeedback
-        testID={touchableId}
-        onPress={() => showModal(config)}
-      >
-        <View></View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
-  const ComponentWithModal = withModal(Component);
-  const { getByTestId } = rawRender(<ComponentWithModal />);
+  const { getByTestId } = renderModalBase(config);
 
-  fireEvent(getByTestId(touchableId), 'press');
+  fireEvent(getByTestId(showModalButtonId), 'press');
 
   const [btnOne] = config.actions;
 
