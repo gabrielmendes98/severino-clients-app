44b4734d2b10297e1468e5825c5c0f176ae0b47c
Gabriel <gabriel.santiago@tqi.com.br>
2022-05-15 22:06:35 -0300

    test form

--- src/common/contexts/Form/__tests__/index.jsx
+++ src/common/contexts/Form/__tests__/index.jsx
@@ -1,3 +1,43 @@
-it('should be able to pass a children as function, exposing form methods', () => {});
+import React from 'react';
+import { Button } from 'react-native';
+import { rawRender, fireEvent, waitFor } from 'test-utils';
+import TextInput from 'components/Input/TextInput';
+import Form from '../index';
 
-it('should be able to pass a children as component', () => {});
+it('should be able to pass a children as function, exposing form methods', async () => {
+  const buttonText = 'submit';
+  const data = {
+    testField: 'dale dale',
+  };
+  const submit = jest.fn();
+  const { getByText } = rawRender(
+    <Form defaultValues={{ testField: data.testField }}>
+      {({ handleSubmit }) => (
+        <>
+          <TextInput name="testField" />
+          <Button
+            onPress={handleSubmit(values => submit(values))}
+            title={buttonText}
+          />
+        </>
+      )}
+    </Form>,
+  );
+
+  fireEvent(getByText(buttonText), 'press');
+  await waitFor(() => {
+    expect(submit).toHaveBeenCalledWith(data);
+  });
+});
+
+it('should be able to pass a children as component', () => {
+  const buttonText = 'submit';
+  const submit = jest.fn();
+  const { getByText } = rawRender(
+    <Form defaultValues={{ testField: '' }}>
+      <TextInput name="testField" />
+      <Button onPress={submit} title={buttonText} />
+    </Form>,
+  );
+  expect(getByText(buttonText)).toBeTruthy();
+});
